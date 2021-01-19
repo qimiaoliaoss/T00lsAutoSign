915642a51d07c5520afff4049fe0da30829beeda
iSec <isafe@users.noreply.github.com>
2021-01-19 19:52:32 +0800

    Update t00ls.py

--- t00ls.py
+++ t00ls.py
@@ -10,7 +10,6 @@ import logging
 logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s - %(filename)s [line:%(lineno)d] - %(levelname)s: %(message)s')
 # usage:
-# logging.info('this is a loggging info message')
 # logging.debug('this is a loggging debug message')
 # logging.warning('this is loggging a warning message')
 # logging.error('this is an loggging error message')
